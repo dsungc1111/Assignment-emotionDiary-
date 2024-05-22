9caaea63d7ceda0f8c542b905c391304c2fd5c7e
dsungc1111 <eotjd9808@naver.com>
2024-05-22 18:49:32 +0900

    -감정다이어리 1. 감정버튼 눌렀을때 카운트 - o 2. 앱 껐다 켰을 때, 카운트 유지  - o 3. 카운트 유지한 상태에서 감정 버튼 눌렀을 때 정상적 카운트 - o 4. 리셋 버튼 작상 작동, 누른 후 어플 다시 열었을 때 카운트 0 유지 - o

--- SeSAC0517Assignment/pageGroup/ViewController.swift
+++ SeSAC0517Assignment/pageGroup/ViewController.swift
@@ -46,16 +46,32 @@ class ViewController: UIViewController {
     @IBOutlet var sadButton: UIButton!
     @IBOutlet var sadLabel: UILabel!
     
+    @IBOutlet var resetButton: UIButton!
+    
+    
+    
     var count = [0,0,0,0,0,0,0,0,0]
+    
     var emotion = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "걱정해", "눈물나"]
     var cuteSlime: [UIImage] = [.slime1, .slime2, .slime3, .slime4, .slime5, .slime6, .slime7, .slime8, .slime9]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let value = UserDefaults.standard.bool(forKey: "0번")
+//        if value {
+//            for i in 0...labelList.count-1 {
+//                count[i] = 0
+//            }
+//        } else {
+//            for i in 0...labelList.count-1 {
+//                count[i] = UserDefaults.standard.integer(forKey: "0\(i)번")
+//            }
+//        }
+
         backgroundImage()
         emotionButton()
         emotionLabel()
-
+        getLastRecord()
     }
 
     
@@ -69,6 +85,7 @@ class ViewController: UIViewController {
         for i in 0...labelList.count-1 {
             buttonList[i].setImage(cuteSlime[i], for: .normal)
         }
+        resetButton.setImage(.icon, for: .normal)
     }
     
     func emotionLabel() {
@@ -77,13 +94,33 @@ class ViewController: UIViewController {
         }
     }
     
+    func getLastRecord() {
+        for i in 0...labelList.count-1 {
+            let number = UserDefaults.standard.integer(forKey: "\(i)번")
+            count[i] = number
+            labelList[i].text = "\(emotion[i]) \(count[i])"
+        }
+    }
+    
+    
+    
     @IBAction func emotionButtonTapped(_ sender: UIButton) {
         count[sender.tag] += 1
         labelList[sender.tag].text = "\(emotion[sender.tag]) \(count[sender.tag])"
         
+        UserDefaults.standard.setValue(count[sender.tag], forKey: "\(sender.tag)번")
     }
     
     
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+        for i in 0...labelList.count-1 {
+            count[i] = 0
+            UserDefaults.standard.setValue(count[i], forKey: "\(i)번")
+            labelList[i].text = "\(emotion[i]) \(count[i])"
+        }
+        
+    }
     
     
     
